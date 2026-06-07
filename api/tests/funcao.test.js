89dafcc7802e15c60195550c7954775d4c89ab32
camilajs <cc25317@g.unicamp.br>
2026-06-06 22:42:28 -0300

    Arrumando arquivo

--- api/tests/funcao.test.js
+++ api/tests/funcao.test.js
@@ -3,13 +3,11 @@ const { calcularIMC } = require('../src/funcao');
 describe('Teste com valores corretos', () => {
 
     test('Valores incorretos', () => {
-        expect(() => calcularIMC(-1, 10))
-            .toThrow('Valor Errado');
+        expect(() => calcularIMC(-1, 10)).toThrow('Valor Errado');
     });
 
     test('Valores corretos', () => {
-        expect(calcularIMC(64, 1.60))
-            .toBe(25);
+        expect(calcularIMC(64, 1.60)).toBeCloseTo(25, 5);
     });
 
 });
